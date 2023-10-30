20b75bf602db557e70caf5e13c0588ae5a7b82ea
Farhan <142213240+farhans-logic@users.noreply.github.com>
2023-10-30 04:29:19 +0000

    feat: Complete Ex 2 fetch api

--- exercise001.js
+++ exercise001.js
@@ -13,9 +13,31 @@ const promise = new Promise((resolve, reject) => {
 
 // Your solution(s) to exercise001 here!
 
+// Using .then.catch chaining
 promise.then((response) => {
-	console.log(`Yay! Promise resolved with response: ${response}`);
+	console.log(`Yay! Promise resolved with response: ${response}`)
 })
 .catch((error) => {
     console.log(`Boo. Promise rejected with response: ${error}`);
 })
+
+// Async Await with try catch
+const getPromiseResponse = async () => {
+	try {
+		const response = await promise;
+		console.log(`Yay! Promise resolved with response: ${response}`);
+	} catch (error) {
+		console.log(`Boo. Promise rejected with response: ${error}`);
+	}
+};
+
+getPromiseResponse();
+
+
+// Top level Await ES12 (2022)
+try {
+	const response = await promise;
+	console.log(`Yay! Promise resolved with response: ${response}`);
+} catch (error) {
+	console.log(`Boo. Promise rejected with response: ${error}`);
+}
